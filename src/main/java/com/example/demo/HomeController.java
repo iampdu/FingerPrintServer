be77a7b6cee07604d0a542df24e9e54e8c92bc8f
AMITPANDEY-LPT\Administrator1 <Prasad.uplenchwar@idmworks.com>
2019-09-12 17:15:41 +0530

    change in controller

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -5,11 +5,19 @@ import java.io.IOException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author Prasad u 
+ * This is an controller use for calling Finger Print Servers.
+ *
+ */
 @RestController
 public class HomeController {
 
+	/**
+	 * This is an method which UI calls to invoke exe from Ui
+	 */
 	@GetMapping("/callexe")
-	public String CallExe() {
+	public void CallExe() {
 
 		try {
 			Process process = new ProcessBuilder("C:\\EHAT.exe").start();
@@ -18,7 +26,6 @@ public class HomeController {
 			e.printStackTrace();
 		}
 
-		return "success";
 	}
 
 }
